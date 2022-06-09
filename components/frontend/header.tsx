5ef569cb50f66a23c6fe82bb05cd6ead2eb25b1c
MoonNight <k0937477331@gmail.com>
2022-06-09 18:04:46 +0800

    style:Change Logo

--- components/frontend/header.tsx
+++ components/frontend/header.tsx
@@ -1,14 +1,25 @@
+import Image from 'next/image';
+import Logo from '../../asset/logo.svg';
 
 const Header = () => {
     return (
         <header className="flex justify-center bg-white">
-            <nav className="w-full max-w-screen-lg px-6 py-4">
-                <h2 className="text-4xl font-semibold text-primary">
-                    LiveIn
-                </h2>
+            <nav className="w-full max-w-screen-lg px-6 py-4 ">
+                <div className="flex items-center">
+                    <Image
+                        src={Logo}
+                        alt="LiveIn logo"
+                        className="stroke-primary fill-primary"
+                        width={32}
+                        height={32}
+                    />
+                    <h2 className="ml-2 text-4xl font-semibold text-primary">
+                        LiveIn
+                    </h2>
+                </div>
             </nav>
         </header>
     )
-}
+};
 
 export default Header
